c5f0cda8ea4fbb5a9f17c2543838b7a48b73641c
DanTheManGude <djg4755@rit.edu>
2017-11-26 22:26:06 -0500

    Firebase, minimal working stage

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 jQuery(document).ready(function ($) {
     $('#alholder').hide();
-    
+
     $('.course-btn').click(function(){
         var cid = $(this).attr('data-course');
         $('#alholder').fadeIn(600);
         console.log(cid);
         writeUserData(cid);
     });
-    
+
     $('#clbtn').click(function(){
         $('#alholder').show().hide();
         console.log("cat");
     });
-    
+
     function writeUserData(cid) {
-      var db = firebase.database();
+        var time = new Date().toLocaleString();
+        firebase.database().ref('times/' + time).set({
+            course: cid
+        });
     }
 });
